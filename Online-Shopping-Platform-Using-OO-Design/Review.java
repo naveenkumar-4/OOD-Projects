07ba6524f01ac77ec2aec41f35c38ca7c2821283
Naveen Kumar <naveenkumarrockz362@gmail.com>
2024-06-07 07:04:23 +0530

    add Review Class

---
+++ Online-Shopping-Platform-Using-OO-Design/Review.java
@@ -0,0 +1,66 @@
+public class Review {
+    private String userId;
+    private String productId;
+    private int rating;
+    private String comment;
+    private int upvotes;
+    private int downvotes;
+
+    public Review(String userId, String productId, int rating, String comment, int upvotes, int downvotes) {
+        this.userId = userId;
+        this.productId = productId;
+        this.rating = rating;
+        this.comment = comment;
+        this.upvotes = upvotes;
+        this.downvotes = downvotes;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getUpvotes() {
+        return upvotes;
+    }
+
+    public void setUpvotes(int upvotes) {
+        this.upvotes = upvotes;
+    }
+
+    public int getDownvotes() {
+        return downvotes;
+    }
+
+    public void setDownvotes(int downvotes) {
+        this.downvotes = downvotes;
+    }
+
+}
