4ecc0c4eb81d2dc6639adecf5b13ed5e6a93aeb2
Naveen Kumar <naveenkumarrockz362@gmail.com>
2024-06-11 06:28:43 +0530

    add Order.java

---
+++ Online-Shopping-Platform-Using-OO-Design/Order.java
@@ -0,0 +1,25 @@
+public class Order {
+    private Cart cart;
+    private PaymentMethod paymentMethod;
+
+    public Order(Cart cart, PaymentMethod paymentMethod){
+        this.cart = cart;
+        this.paymentMethod = paymentMethod;
+    }
+
+    public Cart getCart(){
+        return cart;
+    }
+
+    public void setCart(Cart cart){
+        this.cart = cart;
+    }
+
+    public PaymentMethod getPaymentMethod(){
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod){
+        this.paymentMethod = paymentMethod;
+    }
+}
